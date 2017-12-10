c2cf856a6104a488ff93fdc09985992db05102be
Kirill Shalnov <borhammere@gmail.com>
2017-12-10 22:02:41 +0300

    разметка

--- app/src/main/java/com/example/julia/z/MainActivity.java
+++ app/src/main/java/com/example/julia/z/MainActivity.java
@@ -1,30 +1,44 @@
 package com.example.julia.z;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.CalendarView;
-import android.widget.CalendarView.OnDateChangeListener;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
 public class MainActivity extends AppCompatActivity {
-    private CalendarView calendarView;
+    private MaterialCalendarView calendarView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calendarView = findViewById(R.id.calendar_view);
-        calendarView.setOnDateChangeListener(new OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(),
-                        "Год " + year + "\n" +
-                                "Месяц: " + month + "\n" +
-                                "День: " + dayOfMonth,
-                        Toast.LENGTH_SHORT).show();
 
+        toolbar = findViewById(R.id.toolbar);
+
+        setupToolbar();
+
+        calendarView = findViewById(R.id.calendar_view);
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Toast.makeText(getApplicationContext(),
+                        "Год " + date.getYear() + "\n" +
+                                "Месяц: " + date.getMonth() + "\n" +
+                                "День: " + date.getDay(),
+                        Toast.LENGTH_SHORT).show();
             }
         });
+        calendarView.setTopbarVisible(true);
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
     }
 
 }
