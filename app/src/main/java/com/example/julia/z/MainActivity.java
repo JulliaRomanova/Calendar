52052f17c07edce8f66425876672859090a5325d
JulliaRomanova <33035837+JulliaRomanova@users.noreply.github.com>
2018-01-17 20:12:44 +0300

    Temp

--- app/src/main/java/com/example/julia/z/MainActivity.java
+++ app/src/main/java/com/example/julia/z/MainActivity.java
@@ -8,11 +8,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
-
 public class MainActivity extends AppCompatActivity {
     private MaterialCalendarView calendarView;
     private Toolbar toolbar;
@@ -22,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = findViewById(R.id.toolbar);
         setupToolbar();
         editText = findViewById(R.id.edit_text);
